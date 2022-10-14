3ef51d02f53134d617e3925db39ff1df34bebf22
Melvin RB <melvinruizbatista27.m@gmail.com>
2022-10-14 13:57:06 -0400

    update 1.4, add login

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,58 +3,49 @@ import '../css/Footer.css'
 export default function Footer() {
   return (
     <div>
-       <footer class="site-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12 col-md-6">
-            <h6>About</h6>
-            <p class="text-justify">SOLUTION LIFE is a website help to many people to find a good jobs</p>
-          </div>
+      <footer class="site-footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12 col-md-6">
+              <h6>About</h6>
+              <p class="text-justify">SOLUTION LIFE is a website help to many people to find a good jobs</p>
+            </div>
 
-          <div class="col-xs-6 col-md-3">
-            <h6>Categories</h6>
-            <ul class="footer-links">
-              <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
-              <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
-              <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
-              <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
-              <li><a href="http://scanfcode.com/category/android/">Android</a></li>
-              <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
-            </ul>
-          </div>
+            <div class="col-xs-6 col-md-3">
+              <h6>Categories</h6>
+              <ul class="footer-links">
+                <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
+                <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
+                <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
+                <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
+                <li><a href="http://scanfcode.com/category/android/">Android</a></li>
+                <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
+              </ul>
+            </div>
 
-          <div class="col-xs-6 col-md-3">
-            <h6>Quick Links</h6>
-            <ul class="footer-links">
-              <li><a href="http://scanfcode.com/about/">About Us</a></li>
-              <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
-              <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
-              <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
-              <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
-            </ul>
+            <div class="col-xs-6 col-md-3">
+              <h6>Quick Links</h6>
+              <ul class="footer-links">
+                <li><a href="http://scanfcode.com/about/">About Us</a></li>
+                <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
+                <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
+                <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
+                <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+              </ul>
+            </div>
+          </div>
+          <hr/>
+        </div>
+        <div class="container">
+          <div className="row">
+            <div className="col-md-8 col-sm-6 col-xs-12">
+              <p className="copyright-text">Copyright &copy; 2022 All Rights Reserved by 
+              <a href="#/">Scanfcode</a>.
+              </p>
+            </div>
           </div>
         </div>
-        <hr/>
-      </div>
-      <div class="container">
-        <div className="row">
-          <div className="col-md-8 col-sm-6 col-xs-12">
-            <p className="copyright-text">Copyright &copy; 2022 All Rights Reserved by 
-         <a href="#/">Scanfcode</a>.
-            </p>
-          </div>
-
-          <div class="col-md-4 col-sm-6 col-xs-12">
-            <ul class="social-icons">
-              <li><a className="facebook" href="#/"><i class="fa fa-facebook"></i></a></li>
-              <li><a className="twitter" href="#/"><i class="fa fa-twitter"></i></a></li>
-              <li><a className="dribbble" href="#/"><i class="fa fa-dribbble"></i></a></li>
-              <li><a className="linkedin" href="#/"><i class="fa fa-linkedin"></i></a></li>   
-            </ul>
-          </div>
-        </div>
-      </div>
-</footer>
+      </footer>
     </div>
   )
 }
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,7 +1,88 @@
-import React from 'react'
+import "../css/login.css";
+import { useState } from "react";
 
-export default function Login() {
-  return (
-    <div>login</div>
-  )
+import Swal from "sweetalert2"
+
+
+const Login = () => {
+
+    const [username, setUserName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    var registerData = {
+        "UserName": username,
+        "Email": email,
+        "Password": password
+    }
+
+    var loginData = {
+        "Email": email,
+        "Password": password
+    }
+
+    const successAlert = (msj) =>{
+        Swal.fire({
+          icon: 'success',
+          title: msj,
+          showConfirmButton: false,
+          timer: 1500
+        })
+    }    
+
+    const errorAlert = (msj) =>{
+    
+        Swal.fire({
+        icon: 'error',
+            title: 'Oops...',
+            text: msj,
+        })
+    }
+
+    const formSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <div className="containerLogin" data-aos="fade-left">
+            <div className="main">  	
+                <input type="checkbox" id="chk" aria-hidden="true"/>
+
+                    <div className="signup">
+                        <form>
+                            <label htmlFor="chk" aria-hidden="true">Sign up</label>
+                            <input type="text" name="txt" placeholder="User name" required 
+                            
+                                onChange={(event) => setUserName(event.target.value)}
+                            />
+                            <input type="email" name="email" placeholder="Email" required   
+                            
+                                onChange={(event) => setEmail(event.target.value)}
+                            />
+                            <input type="password" name="pswd" placeholder="Password" required 
+                            
+                                onChange={(event) => setPassword(event.target.value)}
+                            />
+                            <button type="submit">Sign up</button>
+                        </form>
+                    </div>
+
+                    <div className="login">
+                        <form onSubmit={formSubmit}>
+                            <label htmlFor="chk" aria-hidden="true">Login</label>
+                            <input type="email" name="email" placeholder="Email" required 
+                            
+                                onChange={(event) => setEmail(event.target.value)}
+                            />
+                            <input type="password" name="pswd" placeholder="Password" required 
+                            
+                                onChange={(event) => setPassword(event.target.value)}
+                            />
+                            <button className="btnLogin">Login</button>
+                        </form>
+                    </div>
+            </div>
+        </div>
+    )
 }
+export default Login;
